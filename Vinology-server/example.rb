1e03034b473a23ab79d75e185ad75f140b7f7916
Danielcalderon <daniel.calderon@datadoghq.com>
2022-03-19 18:12:51 -0400

    1.1.3_enabling_credential.yml

--- Vinology-server/example.rb
+++ Vinology-server/example.rb
@@ -10,7 +10,7 @@ rescue DatadogAPIClient::V1::APIError => e
 end
 
 DatadogAPIClient::V1.configure do |config|
-    config.api_key = ENV['8bd9d785b5fe5cae4d887735a25e4430']
-    config.application_key = ENV['3a7c7f04556c056e133b12e973018796e130e4f3']
+    config.api_key = ENV[DD_API_KEY]
+    config.application_key = ENV[DD_APP_KEY]
   end
   
